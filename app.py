bc9ce558abf77fc9fcfbe72a67b6d49f2454774b
Cairo Henrique Vaz Cotrim <98197188+Cairo-Henrique@users.noreply.github.com>
2026-04-14 14:50:49 -0300

    Refactor movie recommendation logic in app.py
    Updated movie recommendation logic to include user-selected number of recommendations and improved input handling.

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
-from recommend_movies import get_movie_embedding, recommend_movies, movies
-import os
-import pandas as pd
+from Movies.functions import get_embedding, recommend_movies, movies
 
 app = Flask(__name__, template_folder='templates')
 
@@ -10,13 +8,18 @@ app = Flask(__name__, template_folder='templates')
 def index():
     if request.method == 'POST':
         # pega o texto que o usuário digitou no input do HTML
-        input_description = request.form['description']
+        input_description = request.form['description'].strip()
+        
+        # se o input estiver vazio, mostra a página sem resultados
+        if input_description == '':
+            return render_template('index.html', results=None)
+
+        # pega o número de filmes que o usuário seleicionou no dropdown do HTML
+        top_n = int(request.form['top_n'])
 
         # gera embedding e recomendações
-        input_embedding = get_movie_embedding(input_description,
-                                              input_description,
-                                              input_description)
-        recommended_movies = recommend_movies(input_embedding, movies, top_n=5)
+        input_embedding = get_embedding(input_description)
+        recommended_movies = recommend_movies(input_embedding, movies, style='title_overview_genres', top_n=top_n)
 
         # transforma em lista para passar ao HTML
         output = []
